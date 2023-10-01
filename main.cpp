f3136978e474a84e768d4fc2498440bf9e191029
vbagonas <76889117+vbagonas@users.noreply.github.com>
2023-10-01 18:32:58 +0300

    Add files via upload
    rezultatai išvedami pagal pateiktą pavyzdį ir galutinis vidurkis su 2 skaičiais po kablelio tikslumu

--- main.cpp
+++ main.cpp
@@ -1,22 +1,29 @@
 #include <iostream>
-
+#include <iomanip>
 using namespace std;
 
 int main()
 {
     string vard;
     string pav;
-    int namdarb, egz;
-    double vid;
+    int namdarb, namdarb2, namdarb3, egz, n = 3;
+    double vid, ndvid;
     cout<<"iveskite varda: ";
     cin>>vard;
     cout<<"iveskite pavarde: ";
     cin>>pav;
-    cout<<"iveskite ND gauta pazymi: ";
+    cout<<"iveskite ND1 gauta pazymi: ";
     cin>>namdarb;
+    cout<<"iveskite ND2 gauta pazymi: ";
+    cin>>namdarb2;
+    cout<<"iveskite ND3 gauta pazymi: ";
+    cin>>namdarb3;
     cout<<"iveskite egzamino pazymi: ";
     cin>>egz;
-    vid =0.4*namdarb + 0.6*egz;
-    cout<<vard<<" "<<pav<<" "<<vid<<endl;
+    ndvid = (namdarb+namdarb2+namdarb3)/3.0;
+    vid =(0.40*ndvid) + (0.60*egz);
+    cout<<setw(15)<<left<<"Vardas"<<setw(15)<<"Pavarde"<<"Galutinis (Vid.)"<<endl;
+    cout<<"----------------------------------------------"<<endl;
+    cout<<setw(15)<<left<<vard<<setw(15)<<pav<<setprecision(3)<<vid<<endl;
     return 0;
 }
