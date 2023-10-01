155c03c0089b6a3c513621a6555ee71e49a5aafb
vbagonas <76889117+vbagonas@users.noreply.github.com>
2023-10-01 21:36:53 +0300

    Add files via upload
    Panaudojau struktūra bei vektorius, taip pat vartotojas nusprendžia kiek bus studentų ir kiek bus namų darbų

--- main.cpp
+++ main.cpp
@@ -1,29 +1,59 @@
 #include <iostream>
 #include <iomanip>
+#include <vector>
+#include <limits>
 using namespace std;
 
+ struct Studentas {
+        string vardas;
+        string pavarde;
+        vector<int> paz;
+        int egzam;
+        double vidurkis;
+    };
 int main()
 {
-    string vard;
-    string pav;
-    int namdarb, namdarb2, namdarb3, egz, n = 3;
-    double vid, ndvid;
-    cout<<"iveskite varda: ";
-    cin>>vard;
-    cout<<"iveskite pavarde: ";
-    cin>>pav;
-    cout<<"iveskite ND1 gauta pazymi: ";
-    cin>>namdarb;
-    cout<<"iveskite ND2 gauta pazymi: ";
-    cin>>namdarb2;
-    cout<<"iveskite ND3 gauta pazymi: ";
-    cin>>namdarb3;
-    cout<<"iveskite egzamino pazymi: ";
-    cin>>egz;
-    ndvid = (namdarb+namdarb2+namdarb3)/3.0;
-    vid =(0.40*ndvid) + (0.60*egz);
-    cout<<setw(15)<<left<<"Vardas"<<setw(15)<<"Pavarde"<<"Galutinis (Vid.)"<<endl;
-    cout<<"----------------------------------------------"<<endl;
-    cout<<setw(15)<<left<<vard<<setw(15)<<pav<<setprecision(3)<<vid<<endl;
+    vector<Studentas> studentai;
+    int stud_sk;
+    cout<<"iveskite studentu skaiciu: ";
+    cin>>stud_sk;
+
+    for(int i=0; i<stud_sk; i++){
+        Studentas studentas;
+        cout<<"Studentas nr."<<i+1<<" - Ivesk varda: ";
+        cin>>studentas.vardas;
+        cout<<"Studentas nr."<<i+1<<" - Ivesk pavarde: ";
+        cin>>studentas.pavarde;
+        cout<<"Ivesk namu darbu ivertinimus (paspausk bet kokia raide, kad sustabdyti): ";
+        int pazym;
+        while(cin>>pazym){
+            studentas.paz.push_back(pazym);
+        }
+        cin.clear();
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        cout<<"ivesk egzamino pazymi: ";
+        cin>>studentas.egzam;
+        double nd_vid = 0.0;
+        for(int pazym: studentas.paz){
+            nd_vid +=pazym;
+        }
+        if(!studentas.paz.empty()){
+            nd_vid /= studentas.paz.size();
+        }
+        studentas.vidurkis = 0.4 * nd_vid + 0.6 * studentas.egzam;
+        studentai.push_back(studentas);
+    }
+
+    cout<<setw(15)<<left<<"Vardas"<<setw(15)<<"Pavarde"<<"Galutinis vid"<<endl;
+    cout<<"-------------------------------------------"<<endl;
+    for (const Studentas& studentas : studentai) {
+        cout<<setw(15)<<left<<studentas.vardas<<setw(15)<<studentas.pavarde<<fixed<<setprecision(2)<<studentas.vidurkis<<endl;
+    }
+
     return 0;
 }
+
+
+
+
+
