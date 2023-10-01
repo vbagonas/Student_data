7e19a55ae85fe5c2e08faab0248c15fde8380e44
vbagonas <76889117+vbagonas@users.noreply.github.com>
2023-10-01 23:21:05 +0300

    Add files via upload
    Pridėta skaičiavimas su mediana

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <iomanip>
 #include <vector>
 #include <limits>
+#include <algorithm>
 using namespace std;
 
  struct Studentas {
@@ -10,7 +11,21 @@ using namespace std;
         vector<int> paz;
         int egzam;
         double vidurkis;
+        double vidurkis2;
     };
+double Mediana(const vector<int>& pazymiai) {
+    vector<int> rus_pazym = pazymiai;
+    sort(rus_pazym.begin(), rus_pazym.end());
+
+    size_t size = rus_pazym.size();
+    if (size % 2 == 0) {
+        // If even number of elements, return the average of the middle two
+        return (rus_pazym[size / 2 - 1] + rus_pazym[size / 2]) / 2.0;
+    } else {
+        // If odd number of elements, return the middle element
+        return rus_pazym[size / 2];
+    };
+}
 int main()
 {
     vector<Studentas> studentai;
@@ -34,20 +49,23 @@ int main()
         cout<<"ivesk egzamino pazymi: ";
         cin>>studentas.egzam;
         double nd_vid = 0.0;
-        for(int pazym: studentas.paz){
-            nd_vid +=pazym;
+        double nd_med = 0.0;
+        for (int pazym : studentas.paz) {
+            nd_vid += pazym;
         }
         if(!studentas.paz.empty()){
-            nd_vid /= studentas.paz.size();
-        }
+                nd_vid /= studentas.paz.size();
+            }
+        nd_med = Mediana(studentas.paz);
         studentas.vidurkis = 0.4 * nd_vid + 0.6 * studentas.egzam;
+        studentas.vidurkis2 = 0.4 * nd_med + 0.6 * studentas.egzam;
         studentai.push_back(studentas);
     }
 
-    cout<<setw(15)<<left<<"Vardas"<<setw(15)<<"Pavarde"<<"Galutinis vid"<<endl;
-    cout<<"-------------------------------------------"<<endl;
+    cout<<setw(15)<<left<<"Vardas"<<setw(15)<<"Pavarde"<<setw(20)<<"Galutinis (Vid.)"<<"Galutinis (Med.)"<<endl;
+    cout<<"------------------------------------------------------------------"<<endl;
     for (const Studentas& studentas : studentai) {
-        cout<<setw(15)<<left<<studentas.vardas<<setw(15)<<studentas.pavarde<<fixed<<setprecision(2)<<studentas.vidurkis<<endl;
+        cout<<setw(15)<<left<<studentas.vardas<<setw(15)<<studentas.pavarde<<setw(20)<<fixed<<setprecision(2)<<studentas.vidurkis<<fixed<<setprecision(2)<<studentas.vidurkis2<<endl;
     }
 
     return 0;
